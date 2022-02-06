628eeefb5c0dc05a9eb48d3e6f3b0610dba4dcb2
lain <leandro.m9601@gmail.com>
2022-02-05 22:59:44 -0500

    add function dibujarLinea

--- js/canvas.js
+++ js/canvas.js
@@ -1,7 +1,16 @@
 let pincel = ahorcado.getContext("2d");
-pincel.fillStyle = "red";
+pincel.fillStyle = "#C8F2EF";
 pincel.fillRect(0,0,1200,800);
 
-/* function dibujarLinea(color, xi, yi, xf, yf) {
-} */
+function dibujarLinea (color, xinicial, yinicial, xfinal, yfinal) {
+	pincel.beginPath(); // para arrancar un trazo. empezar a dibujar
+	pincel.strokeStyle = color; // variable para asignar color
+	pincel.moveTo(xinicial, yinicial); // función para colocar lapiz partida
+	pincel.lineTo(xfinal, yfinal); // función para colocar lapiz llegada
+	pincel.stroke(); // dibuja la línea
+	pincel.closePath(); // levantar el lapiz para finalizar
+}
+
+
+dibujarLinea("red", 20, 20, 50, 20);
 
